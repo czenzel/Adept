773269f97fbcb82c5da9e4141246015df3d703a8
Jared Bienz <jbienz@hotmail.com>
2016-03-30 20:24:03 -0500

    EditorSample added to test UnityView control. Compiling but not running. Something about Unprocessed assemblies not making it to the output folder or not getting referenced correctly.

---
+++ Samples/UWP/EditorSample/EditorSample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Adept.UnityXaml;
+using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EditorSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void CubeButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Unity.LoadSceneAsync("CubeScene");
+            await new MessageDialog("Loaded").ShowAsync();
+        }
+
+        private async void Sphere_Click(object sender, RoutedEventArgs e)
+        {
+            await Unity.LoadSceneAsync("SphereScene");
+            await new MessageDialog("Loaded").ShowAsync();
+        }
+    }
+}
